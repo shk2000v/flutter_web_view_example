b6bfcd4d60a85b08cd40204e02a9395ac80373b8
shk <shk2000v@gmail.com>
2023-12-19 02:47:55 +0900

    first commit

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+final homeUrl = Uri.parse('https://blog.codefactory.ai');
+
+class HomeScreen extends StatelessWidget {
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(homeUrl);
+
+  HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'CodeFactory',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.orange,
+        actions: [
+          IconButton(
+              onPressed: () {
+                controller.loadRequest(homeUrl);
+              },
+              icon: Icon(
+                Icons.home,
+              ))
+        ],
+      ),
+      body: WebViewWidget(
+        controller: controller,
+      ),
+    );
+  }
+}
